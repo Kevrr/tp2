337c52fae6a8f1943dae988a9bed129a34720822
Kevin <krr284@gmail.com>
2021-01-11 16:10:06 -0600

    funciones para los pilotos

--- tp2.py
+++ tp2.py
@@ -1,11 +1,30 @@
 from tkinter import *
+from PIL import Image, ImageTk
+from random import randint as randint
+import pickle
+
+class Pilot:
+    name = ""
+    img = ""
+    hs = 0
+
+    def __init__(self, name, img):
+        self.name = name
+        self.img = img
 
 class MainWindow:
+    pilots = []
 
     def __init__(self, master):
         self.master = master
         self.canvas = Canvas(self.master, width = 100, height = 100)
         self.canvas.pack()
+
+        try:
+            self.loadPilots()
+        except:
+            print("no pilots found")
+            self.createPilots()
 
         self.bAbout = Button(self.canvas, text = "Info", command = self.openAbout)
         self.bAbout.pack()
@@ -13,6 +32,25 @@ class MainWindow:
         self.bHighScores.pack()
         self.bSettings = Button(self.canvas, text = "Ajustes", command = self.openSettings)
         self.bSettings.pack()
+
+    def loadImg(self, name):
+        path = os.path.join('img', name)
+        img = Image.open(path)
+        return ImageTk.PhotoImage(img)
+
+    def createPilots(self):
+        names = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        imgs = ["noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg", "noImg.jpg"]
+        for i in range(0, 10):
+            self.pilots.append(Pilot(names[i], imgs[i]))
+            self.pilots[i].hs = randint(0, 334)
+        print("pilots created")
+
+    def loadPilots(self):
+        file = open("pilots.txt", "rb")
+        self.pilots.extend(pickle.load(file))
+        file.close()
+        print("pilots loaded")
 
     def openAbout(self):
         self.master.withdraw()
@@ -53,14 +91,42 @@ class AboutWindow:
         root.deiconify()
 
 class HighScoresWindow:
+    pilots = []
 
     def __init__(self, master):
         self.master = master
+        self.pilots.extend(main.pilots)
         self.canvas = Canvas(self.master, width = 100, height = 100)
         self.canvas.pack()
 
+        self.sortPilots(0, len(self.pilots) - 1)
+        y = 50
+        for i in range(0, 6):
+            self.canvas.create_text(230, y, text = self.pilots[i].name + str(self.pilots[i].hs), anchor = NW)
+            y += 10
+
         self.bBack = Button(self.canvas, text = "atras", command = self.back)
         self.bBack.pack()
+
+    def sortPilots(self, low, high):
+        if low < high:
+            index = self.partition(low, high)
+            self.sortPilots(low, index)
+            self.sortPilots(index + 1, high)
+
+    def partition(self, low, high):
+        pivot = self.pilots[low]
+        while True:
+            while self.pilots[low].hs < pivot.hs:
+                low += 1
+            while self.pilots[high].hs > pivot.hs:
+                high -= 1
+            if low >= high:
+                return high
+            else:
+                self.pilots[low], self.pilots[high] = self.pilots[high], self.pilots[low]
+                low += 1
+                high -= 1
 
     def back(self):
         self.master.destroy()
@@ -73,10 +139,22 @@ class SettingsWindow:
         self.canvas = Canvas(self.master, width = 100, height = 100)
         self.canvas.pack()
 
+        for i in main.pilots:
+            img = loadImage(i.img)
+            self.canvas.create_image(x, y, anchor = NW)
+            self.canvas.create_text(x, y + 50, anchor = NW)
+            x += 50
+            if x == 300:
+                x = 0
+                y += 50
+
         self.bBack = Button(self.canvas, text = "atras", command = self.back)
         self.bBack.pack()
 
     def back(self):
+        file = open("pilots.txt", "wb")
+        pickle.dump(main.pilots, file)
+        file.close()
         self.master.destroy()
         root.deiconify()
 
